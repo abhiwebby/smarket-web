718bf43621a5ff00e154d1218c9562c68127f554
abhiwebby <abhi.webby@gmail.com>
2021-02-07 05:53:32 +0530

    app text updated

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <h4>hello react....</h4>
+      <h4>hello react web app....</h4>
+      <p>lorem ipsum is dummy content....</p>
     </div>
   );
 }
